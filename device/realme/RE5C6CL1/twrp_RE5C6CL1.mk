35ffeb742e3569ac027b8d7fca69e179d220d8de
akashdeep781444 <akashdeepsingh781445@gmail.com>
2026-02-01 17:01:17 +0530

    Create twrp_RE5C6CL1.mk

---
+++ device/realme/RE5C6CL1/twrp_RE5C6CL1.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/realme/RE5C6CL1/device.mk)
+
+PRODUCT_NAME := twrp_RE5C6CL1
+PRODUCT_DEVICE := RE5C6CL1
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3785
+PRODUCT_MANUFACTURER := realme
