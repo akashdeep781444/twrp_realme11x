3ddf2143fe3e67b5f2656e56c1aee0043c4da0ba
akashdeep781444 <akashdeepsingh781445@gmail.com>
2026-02-04 17:38:18 +0530

    Update twrp_RE5C6CL1.mk

--- device/realme/RE5C6CL1/twrp_RE5C6CL1.mk
+++ device/realme/RE5C6CL1/twrp_RE5C6CL1.mk
@@ -1,8 +1,9 @@
-PRODUCT_DEVICE := RE5C6CL1
 PRODUCT_NAME := twrp_RE5C6CL1
+PRODUCT_DEVICE := RE5C6CL1
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme 11x 5G
 PRODUCT_MANUFACTURER := realme
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=RE5C6CL1
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
